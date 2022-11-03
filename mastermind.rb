52642d67ad34406567ea675cfd4fe9fcdb7ae23a
ash0545 <homeashwinsanthosh@gmail.com>
2022-11-03 19:56:44 +0530

    Add Analyze module to count matches between arrays

--- mastermind.rb
+++ mastermind.rb
@@ -1,39 +1,59 @@
 class Game
 
-  GUESSES = 0
+  GUESSES = 6
+  CODE = 1234
 
   def initialize(setter_class, breaker_class)
-
+    @setter = setter_class.new()
+    @breaker = breaker_class.new()
   end
 
-  def play()
-    i = 0
-    while i < GUESSES do
-
+  def play
+    i = 1
+    while i <= GUESSES do
+      puts "Welcome to Mastermind!" if i == 1
+      
+      i += 1
     end
   end
 end
 
 class Player
-  def initialize()
-
-  end
-
-  def setter_input(code)
-
-  end
-
-  def breaker_input(guess)
-
+  def initialize(game)
+    @game = game
   end
 end
 
 class Human < Player # All inputs from user
-
+  
 end
 
 class Computer < Player # All automatic inputs
 
+end
+
+module Analyzer
+  def exact_match(array, comp_array) # returns array with true for exact matches
+    i = -1
+    array.map do |element|
+      i += 1
+      element == comp_array[i]
+    end
+  end
+
+  def match(array, comp_array)
+    exact = exact_match(array, comp_array)
+    i = -1
+    rem_exact = array.reject do |element|
+      i += 1
+      exact[i]
+    end
+    matches = 0
+    rem_exact.each do |element|
+      matches += 1 if comp_array.include?(element)
+    end
+    matches
+  end
 end
 
 Game.new(Computer, Human).play
